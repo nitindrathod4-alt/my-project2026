dfb5bec457a08a4c0a9dbb0d7697fb5087ca0cf8
Nitin Rathod <nitindrathod4@gmail.com>
2026-06-30 13:33:25 +0530

    Added EKS Terraform configuration

--- main.tf
+++ main.tf
@@ -8,242 +8,109 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1" // Specify the AWS region
 }
-
-##########################
-# VARIABLES
-##########################
-
-variable "cluster_name" {
-  type    = string
-  default = "my-cluster"
-}
-
-##########################
-# DEFAULT VPC
-##########################
-
-data "aws_vpc" "default" {
+data "aws_vpc" "default" { // Fetch default VPC
   default = true
 }
+variable "cluster_name" { // Variable for cluster name
+  type    = string
+  default = "my-cluster"
 
-data "aws_subnets" "default" {
+}
+
+data "aws_subnets" "default" { // Fetch  all subnets in the default VPC
   filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    name   = "vpc-id"                  //   Filter by VPC ID
+    values = [data.aws_vpc.default.id] // Filter by default VPC ID
   }
+
 }
 
-##########################
-# EKS CLUSTER ROLE
-##########################
+resource "aws_iam_role" "eks_cluster_role" { // IAM role for EKS cluster
+  name = "eks-cluster-role"                  // Name of the cluster role
 
-resource "aws_iam_role" "eks_cluster_role" {
-
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({ // Assume role policy
     Version = "2012-10-17"
-
     Statement = [{
-      Effect = "Allow"
-
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-
-      Action = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-
-  role       = aws_iam_role.eks_cluster_role.name
-
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" { // Attach EKS cluster policy to the role
+  role       = aws_iam_role.eks_cluster_role.name                // Cluster role name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+} // Attach EKS cluster policy to the role    
 
-}
 
-##########################
-# NODE ROLE
-##########################
+resource "aws_iam_role" "node_role" { // IAM role for EKS worker nodes
+  name = "eks-node-role"              // Name of the node role
 
-resource "aws_iam_role" "node_role" {
-
-  name = "eks-node-role"
-
-  assume_role_policy = jsonencode({
-
+  assume_role_policy = jsonencode({ // Assume role policy
     Version = "2012-10-17"
-
     Statement = [{
-      Effect = "Allow"
-
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-
-      Action = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "node_policies" {
-
-  count = 3
-
-  role = aws_iam_role.node_role.name
+resource "aws_iam_role_policy_attachment" "node_policies" { // Attach necessary policies to the node role
+  count = 3                                                 // Three policies to attach 
+  role  = aws_iam_role.node_role.name                       // Node role name
 
   policy_arn = element([
-
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-
   ], count.index)
-
 }
 
-##########################
-# EKS CLUSTER
-##########################
-
-resource "aws_eks_cluster" "mycluster" {
-
-  name = var.cluster_name
-
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  version = "1.33"
-
-  enabled_cluster_log_types = [
-    "api",
-    "audit",
-    "authenticator"
-  ]
+resource "aws_eks_cluster" "mycluster" {       // EKS Cluster resource
+  name     = var.cluster_name                  // Cluster name
+  role_arn = aws_iam_role.eks_cluster_role.arn // IAM role for the cluster
 
   vpc_config {
-    subnet_ids = data.aws_subnets.default.ids
-  }
-
-  tags = {
-
-    Environment = "Dev"
-
-    Project = "EKS"
-
+    subnet_ids = data.aws_subnets.default.ids // Use all subnets in the default VPC
   }
 
   depends_on = [
-
-    aws_iam_role_policy_attachment.eks_cluster_policy
-
+    aws_iam_role_policy_attachment.eks_cluster_policy // Ensure the cluster role policy is attached before creating the cluster
   ]
-
 }
 
-##########################
-# EKS ADDONS
-##########################
 
-resource "aws_eks_addon" "coredns" {
+resource "aws_eks_node_group" "nodegroup" { // EKS Node Group resource
+  cluster_name    = aws_eks_cluster.mycluster.name
+  node_group_name = "default-node-group"         // Name of the node group
+  node_role_arn   = aws_iam_role.node_role.arn   // IAM role for the node group
+  subnet_ids      = data.aws_subnets.default.ids // Use all subnets in the default VPC
 
-  cluster_name = aws_eks_cluster.mycluster.name
+  instance_types = ["c7i-flex.large"] // Instance type for the worker nodes
 
-  addon_name = "coredns"
-
-}
-
-resource "aws_eks_addon" "kube_proxy" {
-
-  cluster_name = aws_eks_cluster.mycluster.name
-
-  addon_name = "kube-proxy"
-
-}
-
-resource "aws_eks_addon" "vpc_cni" {
-
-  cluster_name = aws_eks_cluster.mycluster.name
-
-  addon_name = "vpc-cni"
-
-}
-
-##########################
-# NODE GROUP
-##########################
-
-OBOBresource "aws_eks_node_group" "nodegroup" {
-OB
-  cluster_name = aws_eks_cluster.mycluster.name
-OB
-  node_group_name = "default-node-group"
-OBOB
-OBOBOB  node_role_arn = aws_iam_role.node_role.arn
-OB
-  subnet_ids = data.aws_subnets.default.ids
-
-OB  instance_types = ["c7i-flex.large"]
-OBOBOB
-OB  capacity_type = "ON_DEMAND"
-
-OB  disk_size = 20
-OB
   scaling_config {
-OBOB
-OB    desired_size = 1
-
-OB    min_size = 1
-
-    max_size = 3
-OBOB
-  }
-
-  tags = {
-
-    Name = "WorkerNodes"
-OB
-    Environment = "Dev"
-
+    desired_size = 1
+    min_size     = 1
+    max_size     = 3
   }
 
   depends_on = [
-
-    aws_iam_role_policy_attachment.node_policies
-
+    aws_iam_role_policy_attachment.node_policies // Ensure node role policies are attached before creating the node group
   ]
-
 }
 
-##########################
-# OUTPUTS
-##########################
-
-output "cluster_name" {
-
+output "cluster_name" { // Output for cluster name
   value = aws_eks_cluster.mycluster.name
-
 }
 
-output "cluster_endpoint" {
-
+output "cluster_endpoint" { //  Output for cluster endpoint
   value = aws_eks_cluster.mycluster.endpoint
-
 }
 
-output "cluster_version" {
 
-  value = aws_eks_cluster.mycluster.version
 
-}
 
-output "cluster_security_group" {
-
-  value = aws_eks_cluster.mycluster.vpc_config[0].cluster_security_group_id
-
-}
